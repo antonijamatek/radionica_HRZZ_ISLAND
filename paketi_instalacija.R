5c6ebd5e0761f5c8453709d27ece86fa7f645f88
Antonija Matek <144710084+antonijamatek@users.noreply.github.com>
2024-05-27 15:01:51 +0200

    Add files via upload

---
+++ paketi_instalacija.R
@@ -0,0 +1,31 @@
+
+#Reading data
+install.packages("readr") #Provides functions to read rectangular data (like CSV files) quickly and efficiently.
+install.packages("openxlsx") #Simplifies the process of reading and writing Excel files.
+
+#Editing data
+install.packages("tidyverse") #A collection of R packages designed for data science, including ggplot2, dplyr, tidyr, readr, purrr, and tibble.
+install.packages("tibble") #A modern take on data frames, providing a cleaner and more readable data structure.
+install.packages("dichromat") #Tools for creating dichromatic color schemes that are accessible to colorblind individuals.
+install.packages("dplyr") #A grammar of data manipulation, providing a consistent set of verbs to solve common data manipulation challenges.
+install.packages("SciViews") #A package providing tools for teaching and applying statistical concepts in R.
+
+#Handling Time-Series Data
+install.packages("xts") #Provides extensible time-series classes and methods.
+install.packages("signal") #A set of signal processing functions.
+
+#Creating Tables from Statistics
+install.packages("broom") #Converts statistical analysis objects from R into tidy data frames.
+
+#Handling Large Datasets
+install.packages("data.table") #An extension of data.frame providing high-performance tools for data manipulation.
+install.packages("reticulate") # Provides an R interface to Python, allowing for seamless interoperability.
+
+#Date and Time Handling
+install.packages("lubridate") #Functions to work with date-times and time zones.
+
+#Analyzing Time-Series
+install.packages("zoo") #Provides infrastructure for regular and irregular time-series data.
+
+#Plotting
+install.packages("plotly") #n interactive graphing library that works well with ggplot2.
